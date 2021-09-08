d1fd1681b5b2bbb57c833331457fae5c09723b1d
Riz <80712058+rizznn@users.noreply.github.com>
2021-09-08 17:26:14 -0400

    create API routes for role, add an employee route to change their role

--- server.js
+++ server.js
@@ -66,6 +66,68 @@ app.get('/api/employee/:id', (req, res) => {
     });
 });
   
+// get all role
+app.get('/api/role', (req, res) => {
+    const sql = `SELECT * FROM role`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+
+// get role by id
+app.get('/api/role/:id', (req, res) => {
+    const sql = `SELECT * FROM role WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
+  
+// Update a employee's role
+app.put('/api/employee/:id', (req, res) => {
+    // we should be extra sure that a role_id was provided before we attempt to update the database.
+    const errors = inputCheck(req.body, 'role_id');
+
+    if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+    }
+
+    const sql = `UPDATE employee SET role_id = ? 
+                 WHERE id = ?`;
+    const params = [req.body.role_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        // check if a record was found
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Employee not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
+    });
+});
+  
 
 // // Delete an employee
 app.delete('/api/employee/:id', (req, res) => {
@@ -88,8 +150,29 @@ app.delete('/api/employee/:id', (req, res) => {
       }
     });
 });
-  
-  
+
+// delete a role
+app.delete('/api/role/:id', (req, res) => {
+    const sql = `DELETE FROM role WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        // checks if anything was deleted
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'role not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+})    
+
 // Create an employee
 app.post('/api/employee', ({ body }, res) => {
     const errors = inputCheck(body, 'first_name', 'last_name', 'role_id', 'manager_id');
