08358b1ec425e1f14b2f3d8ba9a15fb6791a02b4
Riz <80712058+rizznn@users.noreply.github.com>
2021-09-07 22:31:44 -0400

    connected to mysql database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 // PORT designation
@@ -9,6 +10,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'RVNN1704079217',
+      database: 'employees'
+    },
+    console.log('Connected to the employees database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
