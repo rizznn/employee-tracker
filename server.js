290d69b79aa1118b7b6c3308ec4ce56dd7056507
Riz <80712058+rizznn@users.noreply.github.com>
2021-09-07 23:51:11 -0400

    tested GET a single employee, create and delete

--- server.js
+++ server.js
@@ -24,6 +24,37 @@ const db = mysql.createConnection(
     console.log('Connected to the employees database.')
 );
 
+// db.query(`SELECT * FROM employee`, (err, rows) => {
+//     console.log(rows);
+// });  
+// GET a single employee
+db.query(`SELECT * FROM employee WHERE id = 1`, (err, row) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(row);
+});
+
+// // Delete a employee
+// db.query(`DELETE FROM employee WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+// });
+  
+// Create a employee
+const sql = `INSERT INTO employee (id, first_name, last_name, role_id, manager_id) 
+              VALUES (?,?,?,?,?)`;
+const params = [1, 'John', 'Doe', 1, 3];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
